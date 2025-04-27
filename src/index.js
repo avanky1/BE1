fcc2fae7f50e6335f269238da98ba7eed4410af0
Avanish Yadav <saver2024only@gmail.com>
2025-04-27 22:38:59 +0530

    chnage2

---
+++ src/index.js
@@ -0,0 +1,50 @@
+// //require("dotenv").config({path:'./.env'});
+// import connectDB from "./db/index.js";
+
+// import dotenv from "dotenv";
+// // import mongoose from "mongoose";
+// // import { DB_NAME } from "./constants";
+// import connectDB from "./db/index";
+
+// dotenv.config({path:'./.env'});
+// connectDB();
+
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js"; // <-- fixed here
+
+dotenv.config({ path: './.env' });
+connectDB();
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express();
+
+
+(async() => {
+     try{
+         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+         app.on("error", (error)=>{
+          console.log("Error connecting to MongoDB:", error);
+         })
+
+         app.listen(process.env.PORT, () => {
+          console.log(`Server is running on port ${process.env.PORT}`);
+          });
+
+          
+     }catch(error){
+          console.error("Error connecting to MongoDB:", error);
+          throw error;
+     }
+})()
+
+*/
